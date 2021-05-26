f39f367e967f5f934bf21feba7fcb0442e00da2d
Elivious <xamrucs74@gmail.com>
2021-05-26 10:54:18 -0500

    added components and pages folders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import './pages/homepage/homepage.styles.scss';
 
 function App() {
   return (
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './menu-item.styles.scss';
 
-const HomePage = () => (
-    <div className='homepage'>
+const MenuItem = ({title, imageUrl, size}) => (
+    <div style={{
+        backgroundImage: `url(${imageUrl})`
+    }} className={`${size} menu-item`}>
         <div className='directory-menu'>
             <div className='menu-item'>
                 <div className='content'>
-                    <h1 className='title'>HATS</h1>
+                    <h1 className='title'>{title}</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
@@ -13,4 +16,4 @@ const HomePage = () => (
     </div>
 )
 
-export default HomePage;
+export default MenuItem;
