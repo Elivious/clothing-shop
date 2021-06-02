9afc1f206eb69ca7caa9dd78c883e8a0a7f7954d
Elivious <xamrucs74@gmail.com>
2021-06-02 09:53:20 -0500

    modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/directory/directory.component.jsx 	modified:   src/components/menu-item/menu-item.component.jsx 	modified:   src/components/menu-item/menu-item.styles.scss

--- src/App.js
+++ src/App.js
@@ -10,43 +10,11 @@ const HatsPage = () => (
   </div>
 )
 
-const JacketsPage = () => (
-  <div>
-    <h1>JACKETS PAGE</h1>
-    <a href='/'>HOME</a>
-  </div>
-)
-
-const SneakersPage = () => (
-  <div>
-    <h1>SNEAKERS PAGE</h1>
-    <a href='/'>HOME</a>
-  </div>
-)
-
-const WomensPage = () => (
-  <div>
-    <h1>WOMENS PAGE</h1>
-    <a href='/'>HOME</a>
-  </div>
-)
-
-const MensPage = () => (
-  <div>
-    <h1>MENS PAGE</h1>
-    <a href='/'>HOME</a>
-  </div>
-)
-
 function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
-      <Route path='/shop/hats' component={HatsPage} />
-      <Route path='/shop/jackets' component={JacketsPage} />
-      <Route path='/shop/sneakers' component={SneakersPage} />
-      <Route path='/shop/womens' component={WomensPage} />
-      <Route path='/shop/mens' component={MensPage} />
+      <Route path='/hats' component={HatsPage} />
     </div>
   );
 }
