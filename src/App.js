7f29b9ed155739dc9aba0d9164de7269e844ca89
Elivious <xamrucs74@gmail.com>
2021-06-02 12:56:08 -0500

    added sign in page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,8 @@ import './pages/homepage/homepage.styles.scss';
 import {Route, Switch} from 'react-router-dom';
 import ShopPage from './shop/shop.component'
 import Header from './components/header/header.component'
+import SignInAndSignUpPage from './pages/homepage/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-    <a href='/'>HOME</a>
-  </div>
-)
 
 function App() {
   return (
@@ -19,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
